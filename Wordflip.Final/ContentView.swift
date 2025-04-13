fc017fb642fc32917dda0726b211f98b955759a8
Danika Peet <DPeet@lcs.on.ca>
2025-04-13 18:56:39 -0400

    Initial Commit

---
+++ Wordflip.Final/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Wordflip.Final
+//
+//  Created by Danika Peet on 2025-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Rectangle()
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wordflip.Final/Wordflip_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordflip_FinalApp.swift
+//  Wordflip.Final
+//
+//  Created by Danika Peet on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Wordflip_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
